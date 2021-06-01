4a34debe8e097d5e7526152168c85ee7487068b8
dnguyen <david.nguyen@smartsheet.com>
2021-06-01 15:45:35 -0700

    push basic solution codes to github

---
+++ MainTestProject/Main.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MainTestProject
+{
+    [TestClass]
+    public class Main
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+        }
+    }
+}
